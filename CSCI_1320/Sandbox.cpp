8b2c7801151db0cd1105cc90eca28a928188fada
btawesome18 <brtr7823@colorado.edu>
2019-11-25 14:35:42 -0700

    Random Strings

--- CSCI_1320/Sandbox.cpp
+++ CSCI_1320/Sandbox.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <cstdlib>
 
-void swap(int &a, int &b);
+using namespace std;
+
+struct missleSpec {
+  int range;
+  int mass;
+  int speedTop;
+  int Altitude;
+  int payload;
+  bool nuclear;
+  int lenght;
+  string launcher;
+  bool service;
+};
+
 
 int main() {
 
-  int a=4, b=3, c=7, d=2;
+  missleSpec v2;
 
+  v2.range = 200;
+  v2.payload = 2200;
+  v2.speedTop = 3580;
+  v2.lenght = 45;
+  v2.mass = 27600;
+  v2.Altitude = 128;
+  v2.launcher = "Ground based stand";
+  v2.nuclear = 0;
+  v2.service = 0;
 
-}
+  std::cout << "Range (Miles): " << v2.range << '\n';
 
-
-void swap(int &a, int &b){
-  int temp;
-  temp = a;
-  a = b;
-  b = temp;
+  return 0;
 }
