a7435e4cc5fb63a00d7f572e3704fe902772049f
Brian <brtr7823@colorado.edu>
2019-12-04 14:30:22 -0700

    Test

--- CSCI_1320/Project/task1.cpp
+++ CSCI_1320/Project/task1.cpp
@@ -10,12 +10,14 @@ char randLetter();
 
 void buildPopulation(string poulation[], int size, int targetLenght);
 
+int fitnessWord(string test, string target, int length);
+
 int main(){
   srand(time(0));
   int populationSize = 200;
   string population[populationSize];
 
-  buildPopulation(population, populationSize, 10);
+  buildPopulation(population, populationSize, 11);
 
   for (int i = 0; i < 200; i++) {
     std::cout << population[i] << '\n';
@@ -39,4 +41,12 @@ char randLetter(){
   int temp = (rand()%53);
   char alph[] = {" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
   return alph[temp];
+}
+
+int fitnessWord(string test, string target, int length){
+
+  for (int i = 0; i == lenght; i++) {
+
+  }
+
 }
